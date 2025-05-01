cb8466392869a490e01afcdeafcfc673c5a41337
drahmetacik <a.acik@icloud.com>
2025-05-01 23:20:48 +0100

    Enhance ParameterDefinitions with comments for clarity on actor mapping and stage setup

--- src/test/java/starter/stepdefinitions/ParameterDefinitions.java
+++ src/test/java/starter/stepdefinitions/ParameterDefinitions.java
@@ -1,20 +1,24 @@
 package starter.stepdefinitions;
 
-import io.cucumber.java.Before;
-import io.cucumber.java.ParameterType;
-import net.serenitybdd.screenplay.Actor;
-import net.serenitybdd.screenplay.actors.OnStage;
-import net.serenitybdd.screenplay.actors.OnlineCast;
+    import io.cucumber.java.Before;
+    import io.cucumber.java.ParameterType;
+    import net.serenitybdd.screenplay.Actor;
+    import net.serenitybdd.screenplay.actors.OnStage;
+    import net.serenitybdd.screenplay.actors.OnlineCast;
 
-public class ParameterDefinitions {
+    public class ParameterDefinitions {
 
-    @ParameterType(".*")
-    public Actor actor(String actorName) {
-        return OnStage.theActorCalled(actorName);
+        // Define a custom Cucumber parameter type for mapping actor names in feature files to Actor objects
+        @ParameterType(".*")
+        public Actor actor(String actorName) {
+            // Create or retrieve an Actor instance with the given name
+            return OnStage.theActorCalled(actorName);
+        }
+
+        // This method is executed before each scenario to set up the "stage" for the actors
+        @Before
+        public void setTheStage() {
+            // Initialize the "stage" with an OnlineCast, which manages the actors
+            OnStage.setTheStage(new OnlineCast());
+        }
     }
-
-    @Before
-    public void setTheStage() {
-        OnStage.setTheStage(new OnlineCast());
-    }
-}
